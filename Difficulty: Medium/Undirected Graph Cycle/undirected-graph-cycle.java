689ca2e9f8d2e32d362db0807949ca87a7f09140
ayushdabralcoursera2 <ayushdabralcoursera2@gmail.com>
2025-12-08 22:14:12 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Undirected Graph Cycle/undirected-graph-cycle.java
+++ Difficulty: Medium/Undirected Graph Cycle/undirected-graph-cycle.java
@@ -1,38 +1,36 @@
 class Solution {
+    
+    public boolean helper(ArrayList<ArrayList<Integer>> adj, int node, int vis[]) {
+        // System.out.println("HI " + node);
+        int n = adj.get(node).size();
+        for(int i = 0; i < n; i++) {
+            if(vis[adj.get(node).get(i)] == -1) {
+                vis[adj.get(node).get(i)] = node;
+                if(helper(adj, adj.get(node).get(i), vis)) return true;
+            }
+            else if(adj.get(node).get(i) != vis[node]) return true;
+        }
+        return false;
+    }
+    
     public boolean isCycle(int V, int[][] edges) {
         // Code here
-        int m = edges.length;
-        ArrayList<ArrayList<Integer>> adj = new ArrayList<>(V);
-        for(int i = 0; i < V; i++) {
-            adj.add(new ArrayList<>());
+        int vis[] = new int[V];
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+        for(int i = 0 ; i < V; i++) {
+            adj.add(new ArrayList<Integer>());
+            vis[i] = -1; 
         }
-        
+        int m = edges.length;
         for(int i = 0; i < m; i++) {
             adj.get(edges[i][0]).add(edges[i][1]);
             adj.get(edges[i][1]).add(edges[i][0]);
         }
-        
-        int vis[] = new int[V];
-        for(int i = 0; i < V; i++) {
-            vis[i] = -1;
-        }
-        
         for(int i = 0; i < V; i++) {
             if(vis[i] == -1) {
-                Queue<Integer> queue = new LinkedList<>();
-                queue.offer(i);
                 vis[i] = -2;
-                while(!queue.isEmpty()) {
-                    int parent = queue.poll();
-                    for(int child: adj.get(parent)) {
-                        if(vis[child] == -1) {
-                            queue.offer(child);
-                            vis[child] = parent;
-                        }
-                        else if(child != vis[parent]) {
-                            return true;
-                        }
-                    }
+                if(helper(adj, i, vis)) {
+                    return true;
                 }
             }
         }
